be803489edb430a55d2c3d7b39c15e7c10b70d1b
shuguo.zhang <shuguo.zhang@foresight-robotics.com>
2018-08-28 00:12:16 -0700

    build workspace

---
+++ autotest.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+. libautotest
+
+# check the return of the commands
+# success: 0
+# error: not 0
+checkReturn() {
+local ret=$1
+local cmd=$2
+
+if [ ${ret} -eq ${SUCCESSFUL} ]
+then
+    printInfo "${cmd} is OK."
+else
+    printErr "${cmd}: return is ${ret}"
+    printErr "${cmd} is failed."
+    exit ${ret}
+fi
+}
+
+# build Workspace
+setDirectionOfPutout workspace.log
+date
+buildWorkspace
+checkReturn $? buildWorkspace
+mv workspace.log ${LOGDIR}
+resetSTDOUT
+
+# get source from GitLab
+
